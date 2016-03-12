ecde34adfaef52f1c7e048bfff09e24ebef6bc21
Lambros Petrou <lambrospetrou@gmail.com>
2016-03-12 19:30:33 +0000

    Improved test for returned object.

--- test.js
+++ test.js
@@ -8,6 +8,8 @@ expect(typeof LP.ns).to.equal('function')
 var testNamespace = 'my.custom.namespace';
 var testObj = {};
 LP.ns(testNamespace, testObj);
+var deepObj = LP.ns('deep', testObj.my);
 
 expect(testObj).to.have.deep.property(testNamespace);
+expect(testObj.my.deep).to.be.equal(deepObj);
 
